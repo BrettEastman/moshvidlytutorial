153243f3362afb617cc6d803451cd786f58f9462
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-29 19:03:31 -0800

    Add some shape to movies html template and import getMovies method as property to state

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import { getMovies } from '../services/fakeMovieService'; // we need to import this method from the fakeMovieService file, so we go up with '..' then to the file path
 
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies() // then we create a new property in state with this method name to activate it
+  };
+
   render() {
-    return <h2>Movies Component</h2>;
+    return <table className='table'>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   }
 }
 
